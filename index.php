77ffee74d378d820f375a5d081123b377307c585
josearthurlda <josearthurlda@gmail.com>
2025-05-23 20:35:36 -0300

    Modal para adicionar novo candidato feito

--- index.php
+++ index.php
@@ -14,6 +14,43 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
     <div class="container mx-auto p-4">
         <h1 class="text-3xl font-bold mb-4">Lista de Candidatos</h1>
+        <!-- Button trigger modal -->
+        <button type="button" class="btn btn-primary mb-4" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Adicionar Candidato
+        </button>
+
+        <!-- Modal -->
+        
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Dados do Candidato</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+            <form>
+                <div class="mb-3">
+                    <label for="foto" class="form-label">Foto do Candidato</label>
+                    <input class="form-control" type="file" id="foto" name="foto">
+                </div>
+                <div class="mb-3">
+                    <label for="nome" class="form-label">Nome do Candidato</label>
+                    <input type="text" class="form-control" id="nome" name="nome">
+                </div>
+                <div class="mb-3">
+                    <label for="partido" class="form-label">Partido do Candidato</label>
+                    <input type="text" class="form-control" id="partido" name="partido">
+                </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                <button type="submit" class="btn btn-primary">Adicionar</button>
+            </div>
+            </div>
+        </div>
+        </div>
         
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
             <!-- Candidato 1 -->
