fb29d0ab938410a076d0e7433a1c692bb6c15216
josearthurlda <josearthurlda@gmail.com>
2025-05-23 20:15:01 -0300

    Conexão com database

--- index.php
+++ index.php
@@ -6,11 +6,15 @@
     <title>Lista de Candidatos</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <?php
+        require_once "./config/database/database.php";
+    ?>
 </head>
 <body class="bg-gray-100">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
     <div class="container mx-auto p-4">
         <h1 class="text-3xl font-bold mb-4">Lista de Candidatos</h1>
+        
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
             <!-- Candidato 1 -->
             <div class="bg-white rounded-lg shadow-md overflow-hidden">
